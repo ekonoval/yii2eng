c867c2a8af686ad49162917afeaac5014e258c7a
Yehor Konoval <ekonoval@gmail.com>
2015-03-31 13:31:14 +0300

    m

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -4,7 +4,7 @@
 $this->title = 'My Yii Application';
 ?>
 <div class="site-index">
-
+<!--
     <div class="jumbotron">
         <h1>Congratulations!</h1>
 
@@ -12,7 +12,7 @@ $this->title = 'My Yii Application';
 
         <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
     </div>
-
+ -->
     <div class="body-content">
 
         <div class="row">
