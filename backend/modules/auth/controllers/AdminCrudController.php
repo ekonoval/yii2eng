d3c73fba75c4232ae67daaf0b0b57eda76146caf
Yehor Konoval <ekonoval@gmail.com>
2015-03-31 13:40:37 +0300

    admin crud init

---
+++ backend/modules/auth/controllers/AdminCrudController.php
@@ -0,0 +1,13 @@
+<?php
+namespace backend\modules\auth\controllers;
+
+use backend\ext\System\BackendController;
+
+class AdminCrudController extends BackendController
+{
+    public function actionIndex()
+    {
+
+        pa(__CLASS__); exit;
+    }
+}
