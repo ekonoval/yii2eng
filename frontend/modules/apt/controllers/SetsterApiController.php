d390057e51856a437dbe3a2a88dd4f69dc27089d
Yehor Konoval <ekonoval@gmail.com>
2015-04-07 16:14:27 +0300

    sts api fix

--- frontend/modules/apt/controllers/SetsterApiController.php
+++ frontend/modules/apt/controllers/SetsterApiController.php
@@ -9,6 +9,28 @@ class SetsterApiController extends Controller
     public function actionTest()
     {
         $apiObj = new StsApi();
-        $apiObj->auth();
+        $authKey = $apiObj->createAndSetAuthToken();
+
+        //$services = $apiObj->getServices();
+        //pa($services);
+
+        //$accountInfo = $apiObj->getAccountInfo();
+        //$subAcc = $apiObj->getSubAccounts();
+
+        //$employees = $apiObj->getEmployees();
+
+        //$clients = $apiObj->getClients();
+
+        $serviceID = 38271;
+        $providerID = 19237; // 19230
+
+        $params = array(
+            'service_id' => $serviceID,
+            //'provider_id' => $providerID,
+            'start_date' => '2015-04-07',
+            'return' => 'times',
+            't' => 'daily'
+        );
+        $availability = $apiObj->getAvailability($params);
     }
 }
--- frontend/modules/apt/ext/Setster/StsApi.php
+++ frontend/modules/apt/ext/Setster/StsApi.php
@@ -6,9 +6,16 @@ class StsApi
     private $endPointUrl = "http://www.setster.com/api/v2/";
     private $apiKey = '0b51559cda791b9c2031ffb2114d0a3b';
 
+    private $authToken;
+
     private function createCmdUrl($cmdPath, $params = array())
     {
+        $cmdPath = trim($cmdPath, '/');
         $url = $this->endPointUrl . $cmdPath;
+
+        if (!empty($params)) {
+            $url .= '?'.http_build_query($params);
+        }
         return $url;
     }
 
@@ -17,19 +24,41 @@ class StsApi
         StsApiException::ensure($expr, $failMsg);
     }
 
-    private function decodeResponse($responseRaw)
+    private function decodeResponse($responseRaw, $cmdPath)
     {
-        return json_decode($responseRaw, true);
+        $decodedRes = json_decode($responseRaw, true);
+        $this->ensure(!empty($decodedRes), "Something wrong with api result for cmd '{$cmdPath}'");
+        return $decodedRes;
+    }
+
+    private function appendParamsWithAuthToken(&$params)
+    {
+        if (!empty($this->authToken)) {
+            $params["session_token"] = $this->authToken;
+        }
+    }
+
+    private function performGetRequest($cmdPath, $params = array())
+    {
+        $this->appendParamsWithAuthToken($params);
+
+        $cmdUrl = $this->createCmdUrl($cmdPath, $params);
+        $curlPerformer = new CurlPerformer(CurlPerformer::METHOD_GET);
+        $resRaw = $curlPerformer->fileGetContents($cmdUrl);
+
+        $resReady = $this->decodeResponse($resRaw, $cmdPath);
+        return $resReady;
     }
 
     private function performPostRequest($cmdPath, $params)
     {
+        $this->appendParamsWithAuthToken($params);
+
         $cmdUrl = $this->createCmdUrl($cmdPath);
         $curlPerformer = new CurlPerformer(CurlPerformer::METHOD_POST);
         $resRaw = $curlPerformer->fileGetContents($cmdUrl, $params);
 
-        $resReady = $this->decodeResponse($resRaw);
-        $this->ensure(!empty($resReady), "Something wrong with api result for cmd '{$cmdPath}'");
+        $resReady = $this->decodeResponse($resRaw, $cmdPath);
 
         return $resReady;
     }
@@ -51,5 +80,42 @@ class StsApi
         );
 
         return $res["data"]["session_token"];
+    }
+
+    public function createAndSetAuthToken()
+    {
+        $this->authToken = $this->auth();
+    }
+
+    public function getServices()
+    {
+        $res = $this->performGetRequest('service');
+        return $res;
+    }
+
+    public function getAccountInfo()
+    {
+        $res = $this->performGetRequest('account');
+        return $res;
+    }
+
+    public function getSubAccounts()
+    {
+        return $this->performGetRequest('account/companies');
+    }
+
+    public function getEmployees()
+    {
+        return $this->performGetRequest('employee');
+    }
+
+    public function getClients()
+    {
+        return $this->performGetRequest('client');
+    }
+
+    public function getAvailability($params = array())
+    {
+        return $this->performGetRequest('availability', $params);
     }
 }
