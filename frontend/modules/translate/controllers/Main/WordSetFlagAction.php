8cf05d247bb8afb29fbb2c9136993114b4458136
Yehor Konoval <ekonoval@gmail.com>
2015-05-08 14:37:47 +0300

    ajax cb col make dynamic

--- frontend/modules/translate/controllers/Main/WordSetFlagAction.php
+++ frontend/modules/translate/controllers/Main/WordSetFlagAction.php
@@ -2,6 +2,7 @@
 namespace frontend\modules\translate\controllers\Main;
 
 use common\models\Translate\TrWord;
+use frontend\modules\translate\ext\Grid\AjaxCheckboxColumn;
 use yii\base\Action;
 use yii\web\Response;
 
@@ -17,14 +18,30 @@ class WordSetFlagAction extends Action
         $model = null;
         if (!empty($action)) {
             $model = TrWord::findModel($wordID);
+
+            if (in_array($action, [AjaxCheckboxColumn::ACTION_HARD, AjaxCheckboxColumn::ACTION_SUPER_HARD])) {
+                $map = [
+                    AjaxCheckboxColumn::ACTION_HARD => 'isHard',
+                    AjaxCheckboxColumn::ACTION_SUPER_HARD => 'superHard',
+                ];
+                $modelKey = $map[$action];
+
+                $model->$modelKey = $val;
+                $saveRes = $model->save();
+            }
         }
 
-        switch ($action) {
-            case "setHard":
-                $model->isHard = $val;
-                $saveRes = $model->save();
-                break;
-        }
+//        switch ($action) {
+//            case AjaxCheckboxColumn::ACTION_HARD:
+//                $model->isHard = $val;
+//                $saveRes = $model->save();
+//                break;
+//
+//            case AjaxCheckboxColumn::ACTION_SUPER_HARD:
+//                $model->superHard = $val;
+//                $saveRes = $model->save();
+//                break;
+//        }
 
         return $res;
     }
--- frontend/modules/translate/ext/Grid/AjaxCheckboxColumn.php
+++ frontend/modules/translate/ext/Grid/AjaxCheckboxColumn.php
@@ -6,23 +6,27 @@ use yii\helpers\Html;
 
 class AjaxCheckboxColumn extends BooleanColumn
 {
+    const ACTION_HARD = 'setHard';
+    const ACTION_SUPER_HARD = 'superHard';
+
     public $modelKeyUpdate = 'isHard';
+    public $action = self::ACTION_HARD;
+    public $ajaxCheckboxGroupClass = "ajaxCbHard";
 
     public function init()
     {
         parent::init();
 
         $ajaxUrl = "/translate/word-set-flag";
-        $action = "setHard";
 
         $js = <<<JS
-$("input.ajaxCbHard").click(function(){
+$("input.{$this->ajaxCheckboxGroupClass}").click(function(){
     $.getJSON(
         '{$ajaxUrl}',
         {
             wordID: $(this).val(),
             val: $(this).is(':checked') | 0, // bool to int
-            action: '{$action}'
+            action: '{$this->action}'
         },
         function (data) {
             console.log(data);
@@ -37,7 +41,7 @@ JS;
     {
         //return parent::renderDataCellContent($model, $key, $index);
         $isChecked = (bool)$model->{$this->modelKeyUpdate};
-        $html = Html::checkbox('ajaxCb', $isChecked, ['value' => $key, 'class' => 'ajaxCb ajaxCbHard']);
+        $html = Html::checkbox('ajaxCb', $isChecked, ['value' => $key, 'class' => 'ajaxCb '.$this->ajaxCheckboxGroupClass]);
 
         return $html;
     }
